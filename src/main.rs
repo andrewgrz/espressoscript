db5641fcae5f968a5ec9d19af659b5ca5c172068
Andrew <3865404+andrewgrz@users.noreply.github.com>
2020-08-25 08:16:24 -0500

    Improve the cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+use clap::Clap;
+use std::fs;
+use std::io::Read;
+
 mod ast;
 mod grammar;
 
+/// The EspressoScript compiler
+#[derive(Clap)]
+#[clap(
+    version = "pre-alpha",
+    author = "Andrew <3865404+andrewgrz@users.noreply.github.com>"
+)]
+struct Opts {
+    /// Runs the file.
+    input: String,
+    /// Generates the output to here
+    output: String,
+}
+
 fn main() {
-    println!("Welcome to the espressoscript compiler");
-    println!("{:?}", grammar::grammar::module("1+4"));
+    println!("Welcome to the EspressoScript compiler!");
+    let opts: Opts = Opts::parse();
+
+    let mut contents = String::new();
+    let _ = fs::File::open(&opts.input)
+        .expect(&format!("Unable to open: {}", &opts.input))
+        .read_to_string(&mut contents);
+
+    println!("{:#?}", grammar::grammar::module(&contents));
 }
